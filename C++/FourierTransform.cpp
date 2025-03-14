39522ed9b19f6a9d464825be8d6c9af4f8d6eee4
gigacrab <tehwenliang@gmail.com>
2025-03-14 21:06:16 +0800

    DFT modification

--- C++/FourierTransform.cpp
+++ C++/FourierTransform.cpp
@@ -9,6 +9,10 @@ using namespace std;
 typedef complex<double> Complex;
 typedef vector<Complex> cVector;
 
+double roundDp(double n, int dp = 0){
+    return round(n * pow(10, dp)) / pow(10, dp);
+}
+
 int main(){
     int N;
     cout << "Welcome to the *Ultimate* DFT Calculator!!!!!!!!!!!!!!!!\n";
@@ -33,13 +37,14 @@ int main(){
     }
 
     for(int a = 0; a < N; a++){
+        sum = Complex(0, 0);
         for(int i = 0; i < N; i++){
             angle = -2*M_PI*a*i/N;
             sum += vect[i]*Complex(cos(angle), sin(angle));
         }
-        cout << "X[" << a << "] = " << sum << "\n";
+        sum.real(roundDp(sum.real(), 3));
+        sum.imag(roundDp(sum.imag(), 3));
+        cout << "X[" << a << "] = " << sum << ", Magnitude = " << abs(sum) << ", Argument = " << arg(sum) << "\n";
         fBin[a] = sum;
-        sum = Complex(0, 0);
     }
 }
-
