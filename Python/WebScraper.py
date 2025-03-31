6967c3c677895e52a2da46659270d263960454d0
gigacrab <tehwenliang@gmail.com>
2025-03-31 11:51:52 +0800

    modified webscraper

--- Python/WebScraper.py
+++ Python/WebScraper.py
@@ -2,81 +2,78 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+import requests
 import base64
 import time
 import os
+import re
 
-imgCount = 50
-dirName = "unripe-apple-single"
+def scrollToBottom():
+    lastHeight = driver.execute_script('\
+    return document.body.scrollHeight')
+    while True:
+        driver.execute_script('\
+        window.scrollTo(0,document.body.scrollHeight)')
+        time.sleep(3)
+        newHeight = driver.execute_script('\
+        return document.body.scrollHeight')
+        #press the get more images button if its there
+        try:
+            driver.find_element(By.CSS_SELECTOR, ".YstHxe input").click()
+            time.sleep(3)
+        except:
+            pass
+        if newHeight == lastHeight:
+            break
+        lastHeight = newHeight
 
-query = "unripe single apple"
+#query = input("Query(What do you want to search for?): ")
+#dirName = input("Directory Name/Path: ")
+#imgCount = int(input("How many images?: "))
+query = "unripe single banana"
+dirName = "unripe-banana-single"
+imgCount = 40
+
 cService = webdriver.ChromeService(executable_path='/usr/bin/chromedriver')
-chrome_executable_path = '/usr/bin/chromium'
-chrome_options = Options()
-chrome_options.binary_location = chrome_executable_path
-driver = webdriver.Chrome(service=cService, options=chrome_options)
+chromeExecutablePath = '/usr/bin/chromium'
+chromeOptions = Options()
+chromeOptions.binary_location = chromeExecutablePath
+driver = webdriver.Chrome(service=cService, options=chromeOptions)
 driver.maximize_window()
 driver.get('https://images.google.com/')
 box = driver.find_element(By.NAME, "q")
 box.send_keys(query)
 box.send_keys(Keys.ENTER)
 
-def scroll_to_bottom():
-
-    last_height = driver.execute_script('\
-    return document.body.scrollHeight')
-
-    while True:
-        driver.execute_script('\
-        window.scrollTo(0,document.body.scrollHeight)')
-
-        # waiting for the results to load
-        # Increase the sleep time if your internet is slow
-        time.sleep(3)
-
-        new_height = driver.execute_script('\
-        return document.body.scrollHeight')
-
-        # click on "Show more results" (if exists)
-        try:
-            driver.find_element_by_css_selector(".YstHxe input").click()
-
-            # waiting for the results to load
-            # Increase the sleep time if your internet is slow
-            time.sleep(3)
-
-        except:
-            pass
-
-        # checking if we have reached the bottom of the page
-        if new_height == last_height:
-            break
-
-        last_height = new_height
-
-scroll_to_bottom()
+scrollToBottom()
 
 if not os.path.exists(dirName):
     os.makedirs(dirName)
 
+i = 0
+imgIndex = 1
 imgArr = driver.find_elements(By.CLASS_NAME, 'YQ4gaf')
-for i, img in enumerate(imgArr):
-    if i > imgCount: break
-    imgSrc = img.get_attribute("src")
-    # Remove the prefix if it exists
-    if imgSrc.startswith('data:image/jpeg;base64,'):
-        imgSrc = imgSrc[len('data:image/jpeg;base64,'):]
-    print(imgSrc)
+print(f"Found {len(imgArr)} elements!")
+while imgIndex <= imgCount:
+    imgSrc = imgArr[i+5].get_attribute("src")
     if imgSrc:
         try:
-            imgData = base64.b64decode(imgSrc)
-            imgName = f"{dirName}/img-{i}.jpeg"
+            if imgSrc.startswith("https://encrypted-tbn0.gstatic.com/"):
+                imgData = requests.get(imgSrc).content
+            else: 
+                srcSeg = re.split(":|;|,", imgSrc)
+                ext = srcSeg[1].split('/')[1]
+                if not ext == "jpeg": 
+                    i += 1
+                    continue
+                imgData = base64.b64decode(srcSeg[3])
+            imgName = f"{dirName}/img-{imgIndex}.jpeg"
             with open(imgName, "wb") as f:
                 f.write(imgData)
-            #print(f"{imgName} succeesfully written! {imgSrc} \n {imgData}")
+            print(f"{imgName} succeesfully written!")
+            imgIndex += 1
         except Exception as e:
-            print(f"Cant write!!!!: error {e}")
-        
+            print(f"Cant write!!!!: error {e} {imgSrc}")
+    i += 1
 
-# Finally, we close the driver
 driver.close()
