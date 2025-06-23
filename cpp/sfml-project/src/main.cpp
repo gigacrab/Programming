e85242931b7faef53b417c2effcdeb94f99b1c23
gigacrab <tehwenliang@gmail.com>
2025-06-23 13:39:13 +0800

    added circle and text

--- cpp/sfml-project/src/main.cpp
+++ cpp/sfml-project/src/main.cpp
@@ -4,12 +4,39 @@
 
 int main(){
     sf::RenderWindow window;
+    
+    sf::Font font("/home/littlecrabby/Programming/cpp/sfml-project/assets/fonts/Bit-23.ttf");
+    sf::Text text(font);
+    text.setString("Hello world");
+    text.setCharacterSize(24);
+    text.setFillColor(sf::Color::Cyan);
+
+    sf::Texture texture("/home/littlecrabby/Programming/cpp/sfml-project/assets/images/ball.jpg", false, sf::IntRect({0, 0}, {360, 360}));
+    texture.setSmooth(true);
+    texture.setRepeated(true);
+
+    sf::CircleShape circle(50.f);
+    circle.setTexture(&texture  );
+
+
+    sf::Sprite sprite(texture);
+
     window.create(sf::VideoMode({800, 600}), "My Window");
+
     while(window.isOpen()){
         while(const std::optional event = window.pollEvent()){
             if(event->is<sf::Event::Closed>()){
                 window.close();
             }
+
+            sprite.setPosition({10.f, 100.f});
+            text.setPosition({10.f, 10.f});
+            circle.setPosition({400.f, 10.f});
+            window.clear(sf::Color::Black);
+            window.draw(sprite);
+            window.draw(text);
+            window.draw(circle);
+            window.display();
         }
     }
 }
